3d8b4b0f84c10501bef3aff115c0de89bfe517a6
gersongomez <gersongames2017@gmail.com>
2020-03-25 23:19:11 -0600

    altasEmpleados

--- BD NOMINA.sql
+++ BD NOMINA.sql
@@ -1,1 +1,20 @@
 CREATE DATABASE NOMINA;
+use Nomina;
+use Nomina;
+create table baseEmpleados(
+codigoEmpleado int(50) not null primary key,
+nombreEmpleado varchar(10)not null,
+apellidoEmpleado varchar(10)not null,
+sueldoEmpleado varchar(10)not null,
+estadoEmpleado varchar(8)not null)ENGINE=INNODB;
+create table	 altasempleado
+(
+codigoEmpleado int(50),
+nombreEmpleado varchar(10),
+apellidoEmpleado varchar(10),
+sueldoEmpleado varchar(10),
+estadoEmpleado varchar(8)not null,
+foreign key (codigoEmpleado) references baseEmpleados(codigoEmpleado),
+foreign key (nombreEmpleado) references baseEmpleados(codigoEmpleado),
+foreign key (apellidoEmpleado) references baseEmpleados(codigoEmpleado),
+foreign key (sueldoEmpleado) references baseEmpleados(codigoEmpleado))ENGINE=INNODB;
