48ba41ea8c034cf700695e4a4cd041aa79d40072
jeffersondavila <jeffersondavila16@gmail.com>
2020-03-26 00:23:03 -0600

    cheques de los Empleados

--- BD NOMINA.sql
+++ BD NOMINA.sql
@@ -16,3 +16,14 @@ FOREIGN KEY (codigoEmpleado) REFERENCES baseEmpleados(codigoEmpleado),
 FOREIGN KEY (nombreEmpleado) REFERENCES nombreEmpleados(codigoEmpleado),
 FOREIGN KEY (apellidoEmpleado) REFERENCES apellidoEmpleados(codigoEmpleado),
 FOREIGN KEY (sueldoEmpleado) REFERENCES sueldoEmpleados(codigoEmpleado))ENGINE=INNODB;
+CREATE TABLE chequesEmpleados(
+codigocheques varchar (10) primary key,
+nombreEmpleado varchar (10) not null,
+apellidoEmpleado varchar (10) not null,
+sueldoEmpleado varchar (10) not null,
+estadoEmpleado varchar (8) not null,
+foreign key (nombreEmpleado) references baseEmpleados(nombreEmpleado),
+foreign key (apellidoEmpleado) references baseEmpleados(apellidoEmpleado),
+foreign key (sueldoEmpleado) references baseEmpleados(sueldoEmpleado),
+foreign key (estadoEmpleado) references baseEmpleados(sueldoEmpleado)
+) Engine = INNODB;
